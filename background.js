f2dedca4b569bb5a13cd63b75d1bf8b9c5a10b9f
PepijnKruit <61278262+PepijnKruit@users.noreply.github.com>
2020-03-26 18:24:27 +0100

    Finished

--- background.js
+++ background.js
@@ -1,32 +1,33 @@
 var now = new Date();
 var hours = now.getHours();
 
-document.bgColor="#ffffff;";
+document.bgColor = "#ffffff;";
 
-if (hours < 6 && hours < 7){
-document.write ('<body style="background-color: #ffbf4d;">');
+if (hours > 6 && hours < 7){
+	document.write ('<body style="background-color: #ffbf4d;">');
 }
 
 else if (hours > 7 && hours < 12){
-document.write ('<body style="background-color: lightskyblue">');
+	document.write ('<body style="background-color: #c1e3f7;">');
 }
 
 else if (hours > 12 && hours < 14){
-document.write ('<body style="background-color: skyblue">');
+	document.write ('<body style="background-color: #98ceea;">');
 }
 
 else if (hours > 14 && hours < 18){
-document.write ('<body style="background-color: lightskyblue">');
+	document.write ('<body style="background-color: #c1e3f7;">');
 }
 
 else if (hours > 18 && hours < 19){
-document.write ('<body style="background-color: #f4b44d;">');
+	document.write ('<body style="background-color: #f4b44d;">');
 }
 
 else if (hours > 19 || hours < 6){
-document.write ('<body style="background-color: #000000;">');
+	document.write ('<body style="background-color: #000000;">');
 }
 
 else {
-document.write ('<body style="background-color: #ffffff;">');
+	document.write ('<body style="background-color: #ffffff;">');
 }
+
