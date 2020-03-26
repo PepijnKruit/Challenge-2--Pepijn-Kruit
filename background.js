85cb65c068e836a693c5b015080c649bdaf83371
PepijnKruit <61278262+PepijnKruit@users.noreply.github.com>
2020-03-26 06:07:53 +0100

    Final

---
+++ background.js
@@ -0,0 +1,32 @@
+var now = new Date();
+var hours = now.getHours();
+
+document.bgColor="#ffffff;";
+
+if (hours < 6 && hours < 7){
+document.write ('<body style="background-color: #ffbf4d;">');
+}
+
+else if (hours > 7 && hours < 12){
+document.write ('<body style="background-color: lightskyblue">');
+}
+
+else if (hours > 12 && hours < 14){
+document.write ('<body style="background-color: skyblue">');
+}
+
+else if (hours > 14 && hours < 18){
+document.write ('<body style="background-color: lightskyblue">');
+}
+
+else if (hours > 18 && hours < 19){
+document.write ('<body style="background-color: #f4b44d;">');
+}
+
+else if (hours > 19 || hours < 6){
+document.write ('<body style="background-color: #000000;">');
+}
+
+else {
+document.write ('<body style="background-color: #ffffff;">');
+}
